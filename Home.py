c5c6a957fb88c28b1ef7318d9f026018b244b1c6
nat6ta <113652340+nat6ta@users.noreply.github.com>
2022-10-21 11:21:18 +0700

    .commit

--- Home.py
+++ Home.py
@@ -6,3 +6,13 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 st.header("Nantanat")
+
+html_8="""
+<div style="background-color:#EE9513;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>เกณฑ์การให้คะแนน 4</h5></center>
+</div>
+"""
+st.markdown(html_8,unsafe_allow_html=True)
+st.markdown("")
+dx=pd.read_excel('./data/gen.xlsx')
+st.dataframe(dx)
