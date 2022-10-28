ebbcf3f1439b0db346c999786f91e0dbd96e6019
nat6ta <113652340+nat6ta@users.noreply.github.com>
2022-10-28 12:06:20 +0700

    .commit

--- Home.py
+++ Home.py
@@ -31,3 +31,24 @@ if st.button("แสดงการจินตทัศน์ข้อมูล
    st.button("ไม่แสดงข้อมูล")
 else:
     st.write("ไม่แสดงข้อมูล")
+
+html_8="""
+<div style="background-color:#B160EC;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>การทำนายข้อมูลดอกไม้</h5></center>
+</div>
+"""
+
+st.markdown(html_8,unsafe_allow_html=True)
+st.markdown("")
+
+st_len = st.slider("กรุณาเลือกข้อมูล sepal.length")
+sd = st.slider("กรุณาเลือกข้อมูล sepal.width")
+pt_len = st.slider("กรุณาเลือกข้อมูล petal.length")
+wd = st.slider("กรุณาเลือกข้อมูล petal.width")
+
+if  st.button("ทำนายผล]"):
+    st.markdown("ใส่โมเดล")
+    st.button("ไม่แสดงข้อมูล")
+else:
+    st.write("ไม่แสดงผลการทำนาย")
+    
