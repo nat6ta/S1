f40f3a38db1fad7b28495bb786bbb1562b3b5f5a
nat6ta <113652340+nat6ta@users.noreply.github.com>
2022-11-07 19:34:58 +0700

    .commit

--- Home.py
+++ Home.py
@@ -29,7 +29,7 @@ dt6=dt['BMI'].sum()
 dt7=dt['DiabetesPedigreeFunction'].sum()
 dt8=dt['Age'].sum()
 
-dx=[dt1,dt2,dt3,dt4]
+dx=[dt1,dt2,dt3,dt4,dt5,dt6,dt7,dt8]
 dx2=pd.DataFrame(dx,index=["dt1","dt2","dt3","dt4""dt5","dt6","dt7","dt8"])
 
 
