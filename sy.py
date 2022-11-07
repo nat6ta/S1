ea0e63cb50faccab270fe6c99acbff39cdd0f900
nat6ta <113652340+nat6ta@users.noreply.github.com>
2022-11-07 20:27:16 +0700

    Update sy.py

--- sy.py
+++ sy.py
@@ -38,9 +38,35 @@ else:
 
 html_8="""
 <div style="background-color:#B160EC;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
-<center><h5>การทำนายข้อมูลดอกไม้</h5></center>
+<center><h5>การทำนายข้อมูลการเป็นโรคหัวใจ</h5></center>
 </div>
 """
 
 st.markdown(html_8,unsafe_allow_html=True)
 st.markdown("")
+
+st_len = st.slider("กรุณาเลือกข้อมูล Pregnancies")
+sd = st.slider("กรุณาเลือกข้อมูล Glucose")
+pt_len = st.number_input("กรุณาเลือกข้อมูล BloodPressure")
+wd = st.number_input("กรุณาเลือกข้อมูล SkinThickness")
+pt_len = st.number_input("กรุณาเลือกข้อมูล Insulin")
+wd = st.number_input("กรุณาเลือกข้อมูล BMI")
+pt_len = st.number_input("กรุณาเลือกข้อมูล DiabetesPedigreeFunction")
+wd = st.number_input("กรุณาเลือกข้อมูล Age")
+
+if  st.button("ทำนายผล]"):
+    loaded_model = pickle.load(open('./data/trained_model.sav', 'rb'))
+    input_data =  (st_len,sd,pt_len,wd)
+    # changing the input_data to numpy array
+    input_data_as_numpy_array = np.asarray(input_data)
+    # reshape the array as we are predicting for one instance
+    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+    prediction = loaded_model.predict(input_data_reshaped)
+    st.write(prediction[0])
+    if prediction == '1':
+        st.image('./pic/1.jpg')
+    else:
+        st.image('./pic/0.jpg')
+    st.button("ไม่แสดงข้อมูล")
+else:
+    st.write("ไม่แสดงผลการทำนาย")
