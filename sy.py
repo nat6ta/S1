da04d451f60f66413e28ab267a518db78dff8186
nat6ta <113652340+nat6ta@users.noreply.github.com>
2022-11-07 20:17:40 +0700

    Update sy.py

--- sy.py
+++ sy.py
@@ -29,3 +29,18 @@ dt7=dt['DiabetesPedigreeFunction'].sum()
 dt8=dt['Age'].sum()
 dx=[dt1,dt2,dt3,dt4,dt5,dt6,dt7,dt8]
 dx2=pd.DataFrame(dx,index=["dt1","dt2","dt3","dt4","dt5","dt6","dt7","dt8"])
+
+if st.button("แสดงการจินตทัศน์ข้อมูล"):
+   st.pie_chart(dx2)
+   st.button("ไม่แสดงข้อมูล")
+else:
+    st.write("ไม่แสดงข้อมูล")
+
+html_8="""
+<div style="background-color:#B160EC;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>การทำนายข้อมูลดอกไม้</h5></center>
+</div>
+"""
+
+st.markdown(html_8,unsafe_allow_html=True)
+st.markdown("")
