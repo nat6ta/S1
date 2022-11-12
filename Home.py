74e3393d9c4c909cde812e4bc16321debfba2fc8
nat6ta <113652340+nat6ta@users.noreply.github.com>
2022-11-12 10:45:25 +0700

    .commit

--- Home.py
+++ Home.py
@@ -30,7 +30,7 @@ dx=[dt1,dt2,dt3,dt4,dt5,dt6,dt7,dt8]
 dx2=pd.DataFrame(dx,index=["dt1","dt2","dt3","dt4","dt5","dt6","dt7","dt8"])
 
 if st.button("แสดงการจินตทัศน์ข้อมูล"):
-   st.pyplot(dx2)
+   st.line_chart(dx2)
    st.button("ไม่แสดงข้อมูล")
 else:
    st.write("ไม่แสดงข้อมูล")
