92c4af1d5637f5d76e63042bfeefff953a4a5fbf
nat6ta <113652340+nat6ta@users.noreply.github.com>
2022-11-12 11:14:53 +0700

    .commit

--- Home.py
+++ Home.py
@@ -28,10 +28,10 @@ dt7=dt['DiabetesPedigreeFunction'].sum()
 dt8=dt['Age'].sum()
 dx=[dt1,dt2,dt3,dt4,dt5,dt6,dt7,dt8]
 
-dx2=pd.DataFrame(dx,index=["dt1","dt2","dt3","dt4","dt5","dt6","dt7","dt8"],columns=['1','0'])
+dx2=pd.DataFrame(dx,index=["dt1","dt2","dt3","dt4","dt5","dt6","dt7","dt8"])
 
 if st.button("แสดงการจินตทัศน์ข้อมูล"):
-   st.line_chart(dx2)
+   st.area_chart(dx2)
    st.button("ไม่แสดงข้อมูล")
 else:
    st.write("ไม่แสดงข้อมูล")
